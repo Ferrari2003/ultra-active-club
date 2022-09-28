076c8384a38690ee8fe7ced8e8c57e0c818cb15d
Saiful Islam <https://github.com/Ferrari2003>
2022-09-29 02:48:15 +0600

    new commit

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -8,11 +8,11 @@ const Header = () => {
               <h2>Welcome Sports</h2>
             </div>
             <div>
-            <a href="sports">Sports</a>
-                <a href="orders">Orders</a>
+            <a href="login">Login</a>
+                <a href="manage inventory">Manage Inventory</a>
                 <a href="contact">Contact</a>
                 <a href="about">About</a>
-            </div>
+            </div> 
         </div>
     );
 };
