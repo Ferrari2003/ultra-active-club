8f0672232889255d404613b563085cee7e34946e
Saiful Islam <https://github.com/Ferrari2003>
2022-09-29 05:31:33 +0600

    added commit

--- src/component/TextSport/TextSport.js
+++ src/component/TextSport/TextSport.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './TextSport.css'
+import logo from '../../images/png-transparent-sport-logo-football-sports-logos-text-sport-logo.png'
 const TextSport = () => {
     return (
         <div className='Text'>
-            <h1>SPORTS-ULTRA-CLUB</h1>
+            <div className='logo'>
+                <img src={logo} alt="" />
+            <h1 className='text-style'>SPORTS-ULTRA-CLUB</h1>
+            </div>
             <h2>Select today's exercise</h2>
         </div>
     );
